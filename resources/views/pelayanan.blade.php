962a5c2fc9df6e571cc427d36c3ac44cefe6c4fa
Akhyar <ahyar12324@gmail.com>
2023-04-12 17:20:06 +0700

    docs : modify footer in pelayanan
    update to semantic file

--- resources/views/pelayanan.blade.php
+++ resources/views/pelayanan.blade.php
@@ -91,16 +91,13 @@
         <br><br><br>
     </main>
 
-
-    <div style="background-color:white;" class="text-center pt-3">
-        <p>Copyright 2023 &copy; NutriCycle</p>
-    </div>
+    <footer class="bg-white text-center pt-3">
+        <p> Copyright 2023 &copy; NutriCycle</p>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
-
-
 </body>
 
 </html>
