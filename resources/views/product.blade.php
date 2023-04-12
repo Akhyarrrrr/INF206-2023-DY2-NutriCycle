2e3cbccec77e249c26a91eb727ab7f55ce4bd3cc
Akhyar <ahyar12324@gmail.com>
2023-04-12 19:11:35 +0700

    docs : modify format file
    format files to make them tidier

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,131 +2,137 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>NutriCycle</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
-  <script src="https://kit.fontawesome.com/348c676099.js" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>NutriCycle</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/348c676099.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-  <header>
-    <nav class="navbar navbar-expand-lg" style="background-color: #13192B;">
-      <div class="container-fluid">
-        <div class="navbar-brand text-white pe-3" style="border-right: 1px solid white;">
-          <img src="images/logo.png" alt="Logo NutriCycle" width="40">NutriCycle
-        </div>
-        <div class="text-white collapse navbar-collapse">Pembelian Produk</div>
-        <li class="nav-item dropdown" style="margin-right:30px;">
-          <div style="position: relative; bottom:10px;" class="nav-link dropdown-toggle" role="button"
-            data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="fas fa-user-circle fa-2x me-2 text-white"></i>
-            <span class="text-white" style="position: relative;bottom: 5px;">Hi, User</span>
-          </div>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href=""><i class="fas fa-user"></i> Profile</a></li>
-            <li><a class="dropdown-item" href="about.html"><i class="fas fa-info-circle"></i> About Us</a></li>
-            <li><a class="dropdown-item" href="help.html"><i class="fas fa-question-circle"></i> Help</a></li>
-            <li><a class="dropdown-item" href="index.html"><i class="fas fa-sign-out-alt"></i> Log Out</a></li>
-          </ul>
-        </li>
-      </div>
-    </nav>
-  </header>
-
-   <main>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <div class="container">
-      <div class="mx-auto justify-content-center">
-        <div class="d-flex justify-content-center gap-5">
-          <div>
-            <div class="card" style="width: 25rem;">
-              <div class="card-body">
-                <div class="d-flex justify-content-center mb-3">
-                  <img src="images/p1.jpeg" style="width: 200px;">
+    <header>
+        <nav class="navbar navbar-expand-lg" style="background-color: #13192B;">
+            <div class="container-fluid">
+                <div class="navbar-brand text-white pe-3" style="border-right: 1px solid white;">
+                    <img src="images/logo.png" alt="Logo NutriCycle" width="40">NutriCycle
                 </div>
-                <h5 class="card-title">Pakan Ternak</h5>
-                <p class="text-danger">Rp50.000</p>
-                <div class="row mb-3">
-                  <div class="col" style="border-right: 1px solid black;">
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star-half-alt text-warning"></i>4.8
-                  </div>
-                  <div class="col">5 Terjual &nbsp; <i class="far fa-star"></i>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col" style="border-right: 1px solid black;">
-                    <a href="" class="btn"><i class="far fa-2x fa-comments"></i></a>
-                  </div>
-                  <div class="col">
-                    <a href="keranjang.html" class="btn"><i class="fas fa-2x fa-cart-plus"></i></a>
-                  </div>
-                  <div class="col">
-                    <a href="keranjang.html" class="btn btn-primary">Beli Sekarang</a>
-                  </div>
-                </div>
-              </div>
+                <div class="text-white collapse navbar-collapse">Pembelian Produk</div>
+                <li class="nav-item dropdown" style="margin-right:30px;">
+                    <div style="position: relative; bottom:10px;" class="nav-link dropdown-toggle" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user-circle fa-2x me-2 text-white"></i>
+                        <span class="text-white" style="position: relative;bottom: 5px;">Hi, User</span>
+                    </div>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href=""><i class="fas fa-user"></i> Profile</a></li>
+                        <li><a class="dropdown-item" href="about.html"><i class="fas fa-info-circle"></i> About Us</a>
+                        </li>
+                        <li><a class="dropdown-item" href="help.html"><i class="fas fa-question-circle"></i> Help</a>
+                        </li>
+                        <li><a class="dropdown-item" href="index.html"><i class="fas fa-sign-out-alt"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </li>
             </div>
-          </div>
+        </nav>
+    </header>
 
-          <div>
-            <div class="card" style="width: 25rem;">
-              <div class="card-body">
-                <div class="d-flex justify-content-center mb-3">
-                  <img src="images/p2.jpeg" style="width: 200px;">
+    <main>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="container">
+            <div class="mx-auto justify-content-center">
+                <div class="d-flex justify-content-center gap-5">
+                    <div>
+                        <div class="card" style="width: 25rem;">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-center mb-3">
+                                    <img src="images/p1.jpeg" style="width: 200px;">
+                                </div>
+                                <h5 class="card-title">Pakan Ternak</h5>
+                                <p class="text-danger">Rp50.000</p>
+                                <div class="row mb-3">
+                                    <div class="col" style="border-right: 1px solid black;">
+                                        <i class="fas fa-star text-warning"></i>
+                                        <i class="fas fa-star text-warning"></i>
+                                        <i class="fas fa-star text-warning"></i>
+                                        <i class="fas fa-star text-warning"></i>
+                                        <i class="fas fa-star-half-alt text-warning"></i>4.8
+                                    </div>
+                                    <div class="col">5 Terjual &nbsp; <i class="far fa-star"></i>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col" style="border-right: 1px solid black;">
+                                        <a href="" class="btn"><i class="far fa-2x fa-comments"></i></a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="keranjang.html" class="btn"><i
+                                                class="fas fa-2x fa-cart-plus"></i></a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="keranjang.html" class="btn btn-primary">Beli Sekarang</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="card" style="width: 25rem;">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-center mb-3">
+                                    <img src="images/p2.jpeg" style="width: 200px;">
+                                </div>
+                                <h5 class="card-title">Pupuk Organik</h5>
+                                <p class="text-danger">Rp50.000</p>
+                                <div class="row mb-3">
+                                    <div class="col" style="border-right: 1px solid black;">
+                                        <i class="fas fa-star text-warning"></i>
+                                        <i class="fas fa-star text-warning"></i>
+                                        <i class="fas fa-star text-warning"></i>
+                                        <i class="fas fa-star text-warning"></i>
+                                        <i class="fas fa-star-half-alt text-warning"></i>4.8
+                                    </div>
+                                    <div class="col">5 Terjual &nbsp; <i class="far fa-star"></i></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col" style="border-right: 1px solid black;">
+                                        <a href="" class="btn"><i class="far fa-2x fa-comments"></i></a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="keranjang.html" class="btn"><i
+                                                class="fas fa-2x fa-cart-plus"></i></a>
+                                    </div>
+                                    <div class="col"><a href="keranjang.html" class="btn btn-primary">Beli
+                                            Sekarang</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <h5 class="card-title">Pupuk Organik</h5>
-                <p class="text-danger">Rp50.000</p>
-                <div class="row mb-3">
-                  <div class="col" style="border-right: 1px solid black;">
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star-half-alt text-warning"></i>4.8
-                  </div>
-                  <div class="col">5 Terjual &nbsp; <i class="far fa-star"></i></div>
-                </div>
-                <div class="row">
-                  <div class="col" style="border-right: 1px solid black;">
-                    <a href="" class="btn"><i class="far fa-2x fa-comments"></i></a>
-                  </div>
-                  <div class="col">
-                    <a href="keranjang.html" class="btn"><i class="fas fa-2x fa-cart-plus"></i></a>
-                  </div>
-                  <div class="col"><a href="keranjang.html" class="btn btn-primary">Beli Sekarang</a>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-  </main>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    </main>
 
-  <footer style="background-color:white;" class="text-center pt-3">
-    <p>Copyright 2023 &copy; NutriCycle</p>
-  </footer>
+    <footer style="background-color:white;" class="text-center pt-3">
+        <p>Copyright 2023 &copy; NutriCycle</p>
+    </footer>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
