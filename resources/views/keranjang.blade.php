7765a450a485d3d0d17b8611051e1da40416e19b
Ayuauliaaa <ayu_au@mhs.unsyiah.ac.id>
2023-04-13 05:42:15 +0700

    docs : modify main  to semqantic

--- resources/views/keranjang.blade.php
+++ resources/views/keranjang.blade.php
@@ -13,121 +13,105 @@
 
 <body>
 
-  <main>
-
-    <header>
-        <nav class="navbar navbar-expand-lg" style="background-color: #13192B;">
-          <div class="container-fluid">
-            <div class="navbar-brand text-white pe-3" style="border-right: 1px solid white;">
-              <img src="images/logo.png" alt="Logo NutriCycle" width="40">NutriCycle
-            </div>
-            <div class="text-white collapse navbar-collapse">Keranjang</div>
-            <li class="nav-item dropdown" style="margin-right:30px;">
-              <div style="position: relative; bottom:10px;" class="nav-link dropdown-toggle" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-user-circle fa-2x me-2 text-white"></i>
-                <span class="text-white" style="position: relative;bottom: 5px;">Hi, User</span>
+    <main>
+        <header>
+          <nav class="navbar navbar-expand-lg" style="background-color: #13192B;">
+            <div class="container-fluid">
+              <div class="navbar-brand text-white pe-3" style="border-right: 1px solid white;">
+                <img src="images/logo.png" alt="Logo NutriCycle" width="40">NutriCycle
               </div>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href=""><i class="fas fa-user"></i> Profile</a></li>
-                <li><a class="dropdown-item" href="about.html"><i class="fas fa-info-circle"></i> About Us</a></li>
-                <li><a class="dropdown-item" href="help.html"><i class="fas fa-question-circle"></i> Help</a></li>
-                <li><a class="dropdown-item" href="index.html"><i class="fas fa-sign-out-alt"></i> Log Out</a></li>
-              </ul>
-            </li>
+              <div class="text-white collapse navbar-collapse">Keranjang</div>
+              <li class="nav-item dropdown" style="margin-right:30px;">
+                <div style="position: relative; bottom:10px;" class="nav-link dropdown-toggle" role="button"
+                  data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="fas fa-user-circle fa-2x me-2 text-white"></i>
+                  <span class="text-white" style="position: relative;bottom: 5px;">Hi, User</span>
+                </div>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href=""><i class="fas fa-user"></i> Profile</a></li>
+                  <li><a class="dropdown-item" href="about.html"><i class="fas fa-info-circle"></i> About Us</a></li>
+                  <li><a class="dropdown-item" href="help.html"><i class="fas fa-question-circle"></i> Help</a></li>
+                  <li><a class="dropdown-item" href="index.html"><i class="fas fa-sign-out-alt"></i> Log Out</a></li>
+                </ul>
+              </li>
+            </div>
+          </nav>
+        </header>
+    
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="container mb-5">
+          <div class="card p-5 mb-5">
+            <h2 class="text-center">Pakan Ternak</h2>
+            <div class="row">
+              <div class="col-3">
+                <input type="checkbox">
+                <img src="images/p1.jpeg">
+              </div>
+              <div class="col-8">
+                <span class="text-decoration-line-through">Rp80.000</span>
+                <span class="fw-bold text-danger">Rp50.000</span>
+                <br>
+                <br>
+                <span class="text-danger"><b>Promo</b> berakhir dalam 2 hari</span>
+                <br>
+                <br>
+                <div class="d-flex">
+                  <button class="btn">-</button>
+                  <button class="btn">1</button>
+                  <button class="btn">+</button>
+                </div>
+              </div>
+            </div>
           </div>
-        </nav>
-      </header>
-
-    <br><br><br><br>
-
-
-    <div class="container mb-5">
-
-      <div class="card p-5 mb-5">
-        <center>
-          <h2>Pakan Ternak</h2>
-        </center>
-
-        <div class="row">
-          <div class="col-3">
-            <input type="checkbox">
-            <img src="images/p1.jpeg" alt="">
-          </div>
-          <div class="col-8">
-            <span class="text-decoration-line-through">Rp80.000</span>
-            <span class="fw-bold text-danger">Rp50.000</span>
-            <br><br>
-
-            <span class="text-danger"><b>Promo</b> berakhir dalam 2 hari</span>
-
-            <br><br>
-
-            <div class="d-flex">
-              <button class="btn">-</button>
-              <input type="number" style="width:7%;" class="form-control" value="1" placeholder="1">
-              <button class="btn">+</button>
+    
+          <div class="card p-5 mb-5">
+            <h2 class="text-center">Pupuk Organik</h2>
+            <div class="row">
+              <div class="col-3">
+                <input type="checkbox">
+                <img src="images/p2.jpeg" alt="">
+              </div>
+              <div class="col-8">
+                <span class="text-decoration-line-through">Rp160.000</span>
+                <span class="fw-bold text-danger">Rp100.000</span>
+                <br>
+                <br>
+                <span class="text-danger"><b>Promo</b> berakhir dalam 1 hari</span>
+                <br>
+                <br>
+                <div class="d-flex">
+                  <button class="btn">-</button>
+                  <button class="btn">0</button>
+                  <button class="btn">+</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div class="card p-5 mb-5">
-        <center>
-          <h2>Pupuk Organik</h2>
-        </center>
-
-        <div class="row">
-          <div class="col-3">
-            <input type="checkbox">
-            <img src="images/p2.jpeg" alt="">
-          </div>
-          <div class="col-8">
-            <span class="text-decoration-line-through">Rp160.000</span>
-            <span class="fw-bold text-danger">Rp100.000</span>
-            <br><br>
-
-            <span class="text-danger"><b>Promo</b> berakhir dalam 1 hari</span>
-
-            <br><br>
-
-            <div class="d-flex">
-              <button class="btn">-</button>
-              <input type="number" style="width:7%;" class="form-control" value="1" placeholder="1">
-              <button class="btn">+</button>
+    
+        <div style="display:flex;">
+          <div style="float:left;width: 75%;" class="bg-white p-3">
+            <div class="row justify-content-between">
+              <div class="col">
+                <input type="checkbox"> Semua
+              </div>
+              <div class="col" style="text-align: right !important;">
+                Total <b><span class="text-danger">Rp50.000</span></b>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-
-    </div>
-
-    <div style="display:flex;">
-      <div style="float:left;width: 75%;" class="bg-white p-3">
-        <div class="row justify-content-between">
-          <div class="col">
-            <input type="checkbox"> Semua
-          </div>
-          <div class="col" style="text-align: right !important;">
-            Total <b><span class="text-danger">Rp50.000</span></b>
+          <div style="float:right;width: 25%;" class="bg-danger p-3 text-white">
+            Checkout (1)
           </div>
         </div>
-      </div>
-      <div style="float:right;width: 25%;" class="bg-danger p-3 text-white">
-        Checkout (1)
-      </div>
-    </div>
-
-
-
-    <br><br><br>
-
-
-
-    <br><br><br><br><br>
-
-  </main>
-
+        <br>
+        <br>
+        <br>
+        <br>
+      </main>
 
   <div style="background-color:white;" class="text-center pt-3">
     <p>Copyright 2023 &copy; NutriCycle</p>
