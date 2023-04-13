560057289720b58b7e89ccd3afcdc269ac2508f1
Nuzulurrahmah <nuzulurrahmah3899@gmail.com>
2023-04-13 12:43:17 +0700

    docs: add other comment in file index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,6 +26,7 @@
         </nav>
     </header>
 
+    <!-- Menampilkan main halaman-->
     <main>
         <br>
         <br>
@@ -60,6 +61,7 @@
         <p> Copyright 2023 &copy; NutriCycle</p>
     </footer>
 
+    <!-- Memuat file JavaScript dari Bootstrap untuk membuat halaman responsif -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 </body>
