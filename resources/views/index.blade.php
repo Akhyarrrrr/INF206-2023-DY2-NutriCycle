ffd6917a765278f53c93bbea7eebb909bae7b8ce
Akhyar <ahyar12324@gmail.com>
2023-04-13 23:05:57 +0700

    docs : modify link css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <!-- Memuat file CSS dari Bootstrap untuk membuat halaman responsif -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets\css\style.css">
     <!-- Memuat fontawesome untuk ikon -->
     <script src="https://kit.fontawesome.com/348c676099.js" crossorigin="anonymous"></script>
 </head>
