b56f0f1366cc28e07e8f9dcab4cbd1ccae69e028
Akhyar <ahyar12324@gmail.com>
2023-04-13 20:40:36 +0700

    docs : modify format file home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>NutriCycle</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <script src="https://kit.fontawesome.com/348c676099.js" crossorigin="anonymous"></script>
@@ -20,15 +21,19 @@
                 </div>
                 <div class="text-white collapse navbar-collapse">Home</div>
                 <li class="nav-item dropdown" style="margin-right:30px;">
-                    <div style="position: relative; bottom:10px;" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div style="position: relative; bottom:10px;" class="nav-link dropdown-toggle" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fas fa-user-circle fa-2x me-2 text-white"></i>
                         <span class="text-white" style="position: relative;bottom: 5px;">Hi, User</span>
                     </div>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href=""><i class="fas fa-user"></i> Profile</a></li>
-                        <li><a class="dropdown-item" href="about.html"><i class="fas fa-info-circle"></i> About Us</a></li>
-                        <li><a class="dropdown-item" href="help.html"><i class="fas fa-question-circle"></i> Help</a></li>
-                        <li><a class="dropdown-item" href="index.html"><i class="fas fa-sign-out-alt"></i> Log Out</a></li>
+                        <li><a class="dropdown-item" href="about.html"><i class="fas fa-info-circle"></i> About Us</a>
+                        </li>
+                        <li><a class="dropdown-item" href="help.html"><i class="fas fa-question-circle"></i> Help</a>
+                        </li>
+                        <li><a class="dropdown-item" href="index.html"><i class="fas fa-sign-out-alt"></i> Log Out</a>
+                        </li>
                     </ul>
                 </li>
             </div>
@@ -51,7 +56,8 @@
             <div class="row" style="justify-content: center;">
                 <div class="col" style="position: relative;left: 200px;">
                     <a href="pelayanan.html" class="text-decoration-none">
-                        <span style="display: inline-block; height: 250px; width: 250px; background-color: white; border-radius: 100%; overflow: hidden;padding: 50px;">
+                        <span
+                            style="display: inline-block; height: 250px; width: 250px; background-color: white; border-radius: 100%; overflow: hidden;padding: 50px;">
                             <img src="images/home-1.png" style="height: 100%; width: auto;">
                         </span>
                         <p class="text-white">
@@ -65,7 +71,8 @@
                 </div>
                 <div class="col" style="position: relative;right: 200px;">
                     <a href="produk.html" class="text-decoration-none">
-                        <span style="display: inline-block; height: 250px; width: 250px; background-color: white; border-radius: 100%; overflow: hidden;padding: 50px;">
+                        <span
+                            style="display: inline-block; height: 250px; width: 250px; background-color: white; border-radius: 100%; overflow: hidden;padding: 50px;">
                             <img src="images/home-2.png" alt="" style="height: 100%; width: auto;">
                         </span>
                         <p class="text-white">
@@ -89,7 +96,9 @@
         <p> Copyright 2023 &copy; NutriCycle</p>
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </body>
 
 
