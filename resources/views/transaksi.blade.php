cc238fface25d3eefbf5a490b0fb53086550e4e4
Ayu <ayu_au@mhs.unsyiah.ac.id>
2023-05-05 02:06:01 +0700

    docs : update header and head table transaksi

--- resources/views/transaksi.blade.php
+++ resources/views/transaksi.blade.php
@@ -1,5 +1,29 @@
 <x-app-layout>
+<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Data Transaksi
+        </h2>
+    </x-slot>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center mb-2">
+                    <h2 class="mb-0">Tabel Transaksi</h2>
+                </div>
+
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama User</th>
+                            <th>Total Harga</th>
+                            <th>Tanggal</th>
+                            <th>Detail Produk</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
 
 
 
