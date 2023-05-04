1a2f67d5f3bd9306ac5c99fd0a9c1afccfbb42d8
Nuzulurrahmah <nuzulurrahmah3899@gmail.com>
2023-05-04 17:39:38 +0700

    docs: update file adminPemanggilan

--- resources/views/petugas/adminPemanggilan.blade.php
+++ resources/views/petugas/adminPemanggilan.blade.php
@@ -46,7 +46,13 @@
                         Data Kosong
                         @endforelse
                     </tbody>
-
-
-
+                    </table>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+  
 </x-app-layout>
