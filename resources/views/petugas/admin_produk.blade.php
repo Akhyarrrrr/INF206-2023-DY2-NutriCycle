e4c724ef267f676b6d5963b170d962eab2578ea1
sharahiya <050203sharahiya@gmail.com>
2023-05-04 18:44:17 +0700

    docs : modify admin product

--- resources/views/petugas/admin_produk.blade.php
+++ resources/views/petugas/admin_produk.blade.php
@@ -152,20 +152,20 @@
             @endforeach
         </div>
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    @if (session('success'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: '{{ session('success') }}',
+                showConfirmButton: false,
+                timer: 2000
+            })
+        </script>
+    @endif
 
 
 
